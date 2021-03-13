1c726c56a2968a0c823ba7d054cfd981393d3e79
Frank Gao <57463541+FrankQixiangGao@users.noreply.github.com>
2021-03-13 17:04:30 -0600

    add material-ui

---
+++ codebuddy/src/sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './sidebar.css'
+
+function sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>sidebar</h1>
+            <div.sidebar>
+            <h2>SideBar</h2>
+            </div.sidebar>
+        </div>
+    )
+}
+
+export default sidebar
