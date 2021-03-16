32dfe01cea2dddf0f7ed224e1258776c15bba41c
Frank Gao <57463541+FrankQixiangGao@users.noreply.github.com>
2021-03-16 13:31:24 -0500

    Del_Extra_Codes

--- codebuddy/src/sidebar.js
+++ codebuddy/src/sidebar.js
@@ -2,14 +2,13 @@ import React from 'react'
 import './sidebar.css'
 
 function sidebar() {
-    return (
-        <div className="sidebar">
+    return <div className="sidebar">
             <h1>sidebar</h1>
-            <div.sidebar>
+          
             <h2>SideBar</h2>
-            </div.sidebar>
+        
         </div>
-    )
+    
 }
 
 export default sidebar
